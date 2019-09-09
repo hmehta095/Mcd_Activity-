88b336be8f2568c3114c2c7d98edd9cc16939ab9
himanshumehta <himanshumehta@10.51.212.113>
2019-09-09 17:51:01 -0400

    TC1: Subscribe to my Mcd’s

--- McdJunit/McdJunit.java
+++ McdJunit/McdJunit.java
@@ -1,42 +1,58 @@
 import static org.junit.Assert.*;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class McdJunit {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	WebDriver driver;
+	
+	@Before
+	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		
 		driver.get("https://www.mcdonalds.com/ca/en-ca.html");
+		
+		
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	@After
+	public void tearDown() throws Exception {
+		
 	}
 
 	@Test
-	public void testTitle() {
+	public void testTitle() {	
+		
+		// Test case 1 :Title of the subscription feature is “Subscribe to my Mcd’s”
+		
+		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.mcdonalds.com/ca/en-ca.html");
+		WebElement SubscribeText = driver.findElement(By.className("click-before-outline"));
+		String SubscribeTextData = SubscribeText.getText();
+		
+		assertEquals("Subscribe to My McD’s®", SubscribeTextData);
 		
 	}
 
 	
-	@Test
-	public void testPosEmailSignup() {
-		
-	}
-	
-	
-	@Test
-	public void testNegEmailSignup() {
-		
-	}
-	
+//	@Test
+//	public void testPosEmailSignup() {
+//		
+//	}
+//	
+//	
+//	@Test
+//	public void testNegEmailSignup() {
+//		
+//	}
+//	
 	
 	
 }
