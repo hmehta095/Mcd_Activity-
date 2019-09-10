107fdf24cde4b07430ba265226f78b032a1a78d0
himanshumehta <himanshumehta@10.51.212.113>
2019-09-10 14:50:21 -0400

    TC2 popup/offer screen solved

--- McdJunit/McdJunit.java
+++ McdJunit/McdJunit.java
@@ -12,20 +12,20 @@ public class McdJunit {
 
 	WebDriver driver;
 	
-	@Before
-	public void setUp() throws Exception {
+//	@Before
+//	public void setUp() throws Exception {
 //		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
 //		WebDriver driver = new ChromeDriver();
 //		driver.get("https://www.mcdonalds.com/ca/en-ca.html");
 //		
-		
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		
-	}
-
+//		
+//	}
+//
+//	@After
+//	public void tearDown() throws Exception {
+//		
+//	}
+//
 	@Test
 	public void testTitle() {	
 		
@@ -34,6 +34,8 @@ public class McdJunit {
 		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.mcdonalds.com/ca/en-ca.html");
+		WebElement popScreen = driver.findElement(By.cssSelector("a.exit"));
+		popScreen.click();
 		WebElement SubscribeText = driver.findElement(By.className("click-before-outline"));
 		String SubscribeTextData = SubscribeText.getText();
 		
@@ -43,27 +45,42 @@ public class McdJunit {
 
 	
 	@Test
-	public void testPosEmailSignup() {
+	public void testPosEmailSignup() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.mcdonalds.com/ca/en-ca.html");
+
+		WebElement popScreen = driver.findElement(By.cssSelector("a.exit"));
+		popScreen.click();
 		WebElement FirstName = driver.findElement(By.id("firstname2"));
-		FirstName.sendKeys("Shikhar");
+		FirstName.sendKeys("Himanshu Mehta");
 		
 		WebElement Email = driver.findElement(By.id("email2"));
-		Email.sendKeys("hmehta095@gmail.com");
+		Email.sendKeys("simranbal01@gmail.com");
 		
 		WebElement PostCode = driver.findElement(By.className("postalcode"));
-		PostCode.sendKeys("00000M1V");
+		PostCode.sendKeys("    M1V");
 		
 		
 		WebElement SubscribeButton = driver.findElement(By.id("g-recaptcha-btn-2"));
 		SubscribeButton.click();
+		
+//		WebElement VerifyButton = driver.findElement(By.id("recaptcha-verify-button"));
+//		VerifyButton.click();
+		
+
+
+		
+		
 	}
 	
-	
+
+
+
 	@Test
 	public void testNegEmailSignup() {
+
+
 
 	}
 	
