642be34e0e5f17ebebdfa86c579a60ff0fa9e9d9
himanshumehta <himanshumehta@10.51.212.113>
2019-09-10 01:06:25 -0400

    TC2- upto entry valid detail---not complete

--- McdJunit/McdJunit.java
+++ McdJunit/McdJunit.java
@@ -14,10 +14,10 @@ public class McdJunit {
 	
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.mcdonalds.com/ca/en-ca.html");
-		
+//		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("https://www.mcdonalds.com/ca/en-ca.html");
+//		
 		
 	}
 
@@ -42,17 +42,31 @@ public class McdJunit {
 	}
 
 	
-//	@Test
-//	public void testPosEmailSignup() {
-//		
-//	}
-//	
-//	
-//	@Test
-//	public void testNegEmailSignup() {
-//		
-//	}
-//	
+	@Test
+	public void testPosEmailSignup() {
+		System.setProperty("webdriver.chrome.driver","/Users/himanshumehta/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.mcdonalds.com/ca/en-ca.html");
+		WebElement FirstName = driver.findElement(By.id("firstname2"));
+		FirstName.sendKeys("Shikhar");
+		
+		WebElement Email = driver.findElement(By.id("email2"));
+		Email.sendKeys("hmehta095@gmail.com");
+		
+		WebElement PostCode = driver.findElement(By.className("postalcode"));
+		PostCode.sendKeys("00000M1V");
+		
+		
+		WebElement SubscribeButton = driver.findElement(By.id("g-recaptcha-btn-2"));
+		SubscribeButton.click();
+	}
+	
+	
+	@Test
+	public void testNegEmailSignup() {
+
+	}
+	
 	
 	
 }
